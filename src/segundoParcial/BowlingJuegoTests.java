c4fd512ce7e0bde0c106b0f3956c28ad08191fbe
Juan Cruz Martinez <juancruzmart93@gmail.com>
2017-11-27 17:15:48 -0300

    commitUno

---
+++ src/segundoParcial/BowlingJuegoTests.java
@@ -0,0 +1,14 @@
+package segundoParcial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingJuegoTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
